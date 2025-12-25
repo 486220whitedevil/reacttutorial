23220f8bedb3f3bd69b2ed678da24517dc5f896a
Deepak Kewat <deepakkewat768@gmail.com>
2025-12-25 18:16:51 +0530

    Initialize Vite project with React setup, including package.json, main entry files, and basic App component

---
+++ basicreact01/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>This is first react project</h1>
+  );
+}
+
+export default App;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+    <h1>This is first Vite-react project</h1>
+  )
+}
+
+export default App
